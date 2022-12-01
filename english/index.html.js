ab88d91cb566893720b43aa4832e8b9cb68d2ef0
JaegerwaldDev <86529399+JaegerwaldDev@users.noreply.github.com>
2022-12-01 21:13:01 +0100

    commit war crimes and avoided austrian law

---
+++ english/index.html.js
@@ -0,0 +1,6 @@
+function Button_StartLearning () {
+    location.replace("./start_learning.html")
+}
+function Button_GitHub () {
+    window.open("https://github.com/JaegerwaldDev/JaegerwaldDev.github.io/tree/main/english")
+}
---
+++ english/start_learning.html.js
@@ -0,0 +1,3 @@
+function Debug_ReturnToIndex () {
+    location.replace("./index.html")
+}
