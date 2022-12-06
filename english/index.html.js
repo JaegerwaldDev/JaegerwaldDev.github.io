0692221c9bdff5dbf9a9d8620135e40a12cc2a11
JaegerwaldDev <86529399+JaegerwaldDev@users.noreply.github.com>
2022-12-06 09:20:34 +0100

    Update index.html.js

--- english/index.html.js
+++ english/index.html.js
@@ -1,6 +1,6 @@
 function Button_StartLearning () {
-    location.replace("./start_learning.html")
+    location.replace("./start_learning.html");
 }
 function Button_GitHub () {
-    window.open("https://github.com/JaegerwaldDev/JaegerwaldDev.github.io/tree/main/english")
+    window.open("https://github.com/JaegerwaldDev/JaegerwaldDev.github.io/tree/main/english");
 }
