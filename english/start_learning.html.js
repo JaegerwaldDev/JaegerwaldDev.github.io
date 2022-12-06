7f47ea84379d9779f27bd66d59d54f500efe3a01
JaegerwaldDev <86529399+JaegerwaldDev@users.noreply.github.com>
2022-12-06 09:20:58 +0100

    among us is not funny. bottom text.

--- english/start_learning.html.js
+++ english/start_learning.html.js
@@ -1,3 +1,7 @@
 function Debug_ReturnToIndex () {
-    location.replace("./index.html")
+    location.replace("./index.html");
+}
+function Debug_PickRandVocab () {
+    import More3_Data from "./more_3.data.json";
+    window.alert(More3_Data);
 }
