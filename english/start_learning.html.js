3658f3ffe9e34c8558b00861963b250f92699a8f
JaegerwaldDev <86529399+JaegerwaldDev@users.noreply.github.com>
2022-12-06 09:41:43 +0100

    Update start_learning.html.js

--- english/start_learning.html.js
+++ english/start_learning.html.js
@@ -1,8 +1,4 @@
-function Debug_ReturnToIndex () {
-    location.replace("./index.html");
-}
-function Debug_PickRandVocab () {
-    import data from "./more_3.data.json";
-    let MoreThreeData = data;
-    window.alert(MoreThree_Data);
-}
+import data from "./more_3.data.json"; let MoreThreeData = data;
+
+function Debug_ReturnToIndex () { location.replace("./index.html"); }
+function Debug_PickRandVocab () { window.alert(MoreThree_Data); }
