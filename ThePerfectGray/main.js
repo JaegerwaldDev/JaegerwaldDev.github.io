7b763b55ed0606bddc27755f5e6e32827731b640
JaegerwaldDev <86529399+JaegerwaldDev@users.noreply.github.com>
2023-04-08 13:03:43 +0200

    Add files via upload

---
+++ ThePerfectGray/main.js
@@ -0,0 +1,46 @@
+let colors = [];
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+  
+function rgbToHex(r, g, b) {
+    return componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function generateColors (difference, modify_intensity) { 
+    for (let i=0; i<=16; i++) {
+        let value = difference*i;
+        let red = value-(i*modify_intensity);
+        let green = value;
+        let blue = value+(i*2);
+        switch (green) {
+            case 0:
+                break;
+            default:
+                colors.push(rgbToHex(red, green, blue))
+        }
+    }
+}
+
+function setElementsToColors () {
+    let css = document.getElementById("css");
+    for (let i=0; i<=document.getElementsByClassName("first_row")[0].childNodes.length-1; i++) {
+        let colorNode = document.getElementsByClassName("first_row")[0].childNodes[i];
+        let color = colors[colorNode.textContent];
+        colorNode.textContent = "#" + color;
+        css.textContent = css.textContent + "."+"color"+color+"{background-color:#"+color+"}";
+        colorNode.className = "color color" + color;
+    }
+    for (let i=0; i<=document.getElementsByClassName("second_row")[0].childNodes.length-1; i++) {
+        let colorNode = document.getElementsByClassName("second_row")[0].childNodes[i]
+        let color = colors[colorNode.textContent];
+        colorNode.textContent = "#" + color;
+        css.textContent = css.textContent + "."+"color"+color+"{background-color:#"+color+"}";
+        colorNode.className = "color color" + color;
+    }
+} 
+
+generateColors(5, 2);
+setElementsToColors();
